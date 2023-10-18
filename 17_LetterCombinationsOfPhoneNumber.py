f75748206059684d8e864af3dc1c3541a421c405
nivbhaskhar <niv.bhaskar@gmail.com>
2023-10-18 13:31:01 -0700

    clean up

--- 17_LetterCombinationsOfPhoneNumber.py
+++ 17_LetterCombinationsOfPhoneNumber.py
@@ -1,9 +1,50 @@
 #https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 
+
 #Recursion
 from collections import defaultdict
 class Solution:
+
+    from collections import defaultdict
+class Solution:
+    def mapDigitsToLetters(self)->dict[int, list[str]]:
+        mapping = defaultdict(list)
+        number_rep = ord('a')
+        distribution = [3,3,3,3,3,4,3,4]
+        for j in range(2,10):
+            for i in range(distribution[j-2]):
+                mapping[str(j)].append(chr(number_rep))
+                number_rep += 1
+        return mapping
+
+    def letterCombinationsRecursive(self, start_pos: int, digits: list[str], mapping: dict[int, list[str]]) -> List[List[str]]:
+        print(start_pos, digits)
+        candidate_characters = mapping[digits[start_pos]]
+        if start_pos == len(digits)-1:
+            return [[character] for character in candidate_characters]
+        
+        prev_combinations = self.letterCombinationsRecursive(start_pos+1, digits, mapping)
+        new_combinations = []
+        for character in candidate_characters:
+            for combination in prev_combinations:
+                new_combination = combination.copy()
+                new_combination.append(character)
+                new_combinations.append(new_combination)
+        return new_combinations
+        
+        
+        
+
+
+    def letterCombinations(self, digits: str) -> list[str]:
+        mapping = self.mapDigitsToLetters()
+        if len(digits) == 0:
+            return []
+        list_of_digits = [d for d in digits]
+        return [''.join(reversed(x)) for x in self.letterCombinationsRecursive(0, list_of_digits, mapping)]
+    
+
     def __init__(self):
         #Dictionary mapping numbers to letters
         self.d = defaultdict(list)
@@ -16,7 +57,7 @@ class Solution:
                    x += 1
 
 
-    def letterCombinations(self, digits: str, rec=False) -> list[str]:
+    def letterCombinationsOld(self, digits: str, rec=False) -> list[str]:
         if not digits and rec:
             return ['']
         elif not digits:
