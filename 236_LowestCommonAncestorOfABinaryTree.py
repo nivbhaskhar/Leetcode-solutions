3cac44e199a404b50e52b32a94668734fe9b753d
nivbhaskhar <niv.bhaskar@gmail.com>
2023-11-02 18:19:11 -0700

    #236

--- 236_LowestCommonAncestorOfABinaryTree.py
+++ 236_LowestCommonAncestorOfABinaryTree.py
@@ -10,6 +10,43 @@ class TreeNode:
 from collections import deque
 
 class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        processing_stack = [(root,0)]
+        lca_index = None
+        found = {p: False, q:False}
+        while processing_stack:
+            #print(f"{[(node.val, status) for node, status in processing_stack]}, {lca_index}")
+            node, status = processing_stack[-1]
+            if node == p or node == q:
+                found[node] = True
+                num_found = int(found[p]) + int(found[q])
+                if num_found == 1:
+                    lca_index = len(processing_stack)-1
+                    #print(f"lca_val = {processing_stack[lca_index][0].val}")
+                elif num_found == 2:
+                    break
+            
+            node, status = processing_stack.pop()
+
+            if status == 0:
+                processing_stack.append((node, 1))
+                if node.left:
+                    processing_stack.append((node.left, 0))
+            elif status == 1:
+                processing_stack.append((node, 2))
+                if node.right:
+                    processing_stack.append((node.right, 0))
+
+            if lca_index and lca_index >= len(processing_stack):
+                assert lca_index == len(processing_stack)
+                lca_index = len(processing_stack)-1
+                #print(f"reset lca_val = {processing_stack[lca_index][0].val}")
+            
+        if found[p] and found[q]:
+            return processing_stack[lca_index][0]
+        else:
+            return None  
+
     def update_parents_dfs(self, node: 'TreeNode'):
         if node.left:
             self.update_parents(node.left)
